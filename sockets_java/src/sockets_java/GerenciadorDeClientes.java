ec7ec4a1d868cded233e221c3ec1075abd9ee0d9
Izaque Alves <105925377+izaqueAG@users.noreply.github.com>
2024-05-24 16:18:09 -0300

    Update GerenciadorDeClientes.java

--- sockets_java/src/sockets_java/GerenciadorDeClientes.java
+++ sockets_java/src/sockets_java/GerenciadorDeClientes.java
@@ -14,8 +14,8 @@ public class GerenciadorDeClientes extends Thread {
     private String nomeCliente;
     private BufferedReader leitor;
     private PrintWriter escritor;
-    public static final Map<String, GerenciadorDeClientes> clientes = new HashMap<String, GerenciadorDeClientes>();
-    private GerenciadorDeClientes destinatario = null;
+    private static final Map<String, GerenciadorDeClientes> clientes = new HashMap<String,GerenciadorDeClientes>();
+    
 
     public GerenciadorDeClientes(Socket cliente) {
         this.cliente = cliente;
@@ -27,98 +27,82 @@ public class GerenciadorDeClientes extends Thread {
         try {
             leitor = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
             escritor = new PrintWriter(cliente.getOutputStream(), true);
-            escritor.println("Por favor escreva seu nome: ");
-            String msg = leitor.readLine();
-            this.nomeCliente = msg.toLowerCase().replace(",", "");
-            escritor.println("Olá, " + this.nomeCliente);
-            clientes.put(this.nomeCliente, this);
+            efetuarLogin();
+            String msg;
 
             while (true) {
                 msg = leitor.readLine();
-                if (msg.equalsIgnoreCase("::SAIR")) {
+                if (msg.equalsIgnoreCase(Comandos.SAIR)) {
                     this.cliente.close();
-                    break;
-                } else if (msg.toLowerCase().startsWith("::msg")) {
-                    String nomeDestinatario = msg.substring(5).trim();
+                    
+                } else if (msg.startsWith(Comandos.MENSAGEM)) {
+                    String nomeDestinatario = msg.substring(Comandos.MENSAGEM.length(), msg.length());
                     System.out.println("Enviando para " + nomeDestinatario);
-                    destinatario = clientes.get(nomeDestinatario);
+                    GerenciadorDeClientes destinatario = clientes.get(nomeDestinatario);
                     if (destinatario == null) {
                         escritor.println("Cliente não existe");
                     } else {
-                        escritor.println("Aguardando  " + destinatario.getNomeCliente() + " aceitar sua solicitação" );
-                        destinatario.getEscritor().println(this.nomeCliente + " quer se conectar a você. Aceitar conexão? (s/n): ");
-                        
-                     // Usando uma nova thread para aguardar a resposta do destinatário
-                        new Thread(() -> {
-                            try {
-                                String resposta = leitor.readLine();
-                                if (resposta.equalsIgnoreCase("s")) {
-                                    escritor.println(destinatario.getNomeCliente() + " aceitou sua solicitação.");
-                                    destinatario.getEscritor().println("Conexão com " + this.nomeCliente + " estabelecida.");
-                                    ConexaoDireta(destinatario);
-                                } else {
-                                    escritor.println(destinatario.getNomeCliente() + " recusou sua solicitação.");
-                                }
-                            } catch (IOException e) {
-                                escritor.println("Erro ao aguardar resposta do destinatário.");
-                                e.printStackTrace();
-                            }
-                        }).start();
-                    }
-                } else if (msg.equals("::listar-clientes")) {
-                    StringBuffer str = new StringBuffer();
-                    for (String c : clientes.keySet()) {
-                        str.append(c);
-                        str.append(",");
-                    }
-                    if (str.length() > 0) {
-                        str.deleteCharAt(str.length() - 1);
-                    }
-                    escritor.println(str.toString());
+                    	destinatario.getEscritor().println(this.nomeCliente + " disse: " + leitor.readLine());
+                    	}
+                //lista  todos os clientes logados    
+                } else if (msg.equals(Comandos.LISTA_USUARIOS)) {
+                    atualizarListaUsuarios(this);
+                
                 } else {
-                    if (destinatario != null) {
-                        destinatario.getEscritor().println(this.nomeCliente + " disse: " + msg);
-                    } else {
-                        escritor.println(this.nomeCliente + ", você disse: " + msg);
-                    }
+                        escritor.println(this.nomeCliente + ", você disse: " + leitor.readLine());
                 }
-            }
+               }
+            
         } catch (IOException e) {
             System.err.println("O cliente fechou a conexão");
+            clientes.remove(this.nomeCliente);
             e.printStackTrace();
-        } finally {
-            if (nomeCliente != null) {
-                clientes.remove(nomeCliente);
-                if (destinatario != null) {
-                    destinatario.getEscritor().println(this.nomeCliente + " se desconectou.");
-                }
-                System.out.println(nomeCliente + " se desconectou.");
-            }
-        }
+        } 
     }
 
-    public PrintWriter getEscritor() {
+    private synchronized void efetuarLogin() throws IOException {
+		
+		while(true){
+			escritor.println(Comandos.LOGIN);
+			this.nomeCliente = leitor.readLine().toLowerCase().replaceAll(",", "");
+			if(this.nomeCliente.equalsIgnoreCase("null") || this.nomeCliente.isEmpty()){
+				escritor.println(Comandos.LOGIN_NEGADO);
+			}else if(clientes.containsKey(this.nomeCliente)){
+				escritor.println(Comandos.LOGIN_NEGADO);
+			}else{
+				escritor.println(Comandos.LOGIN_ACEITO);
+				escritor.println("olá " + this.nomeCliente);
+				clientes.put(this.nomeCliente, this);
+				for(String cliente: clientes.keySet()){
+					atualizarListaUsuarios(clientes.get(cliente));
+				}
+				break;
+			}
+		}
+	}
+
+	private void atualizarListaUsuarios(GerenciadorDeClientes cliente) {
+    	StringBuffer str = new StringBuffer();
+        for (String c : clientes.keySet()) {
+        	if(cliente.getNomeCliente().equals(c))
+				continue;
+			
+			str.append(c);
+			str.append(",");
+        }
+        if(str.length() > 0)
+			str.delete(str.length()-1, str.length());
+		cliente.getEscritor().println(Comandos.LISTA_USUARIOS);
+		cliente.getEscritor().println(str.toString());
+		
+	}
+
+	public PrintWriter getEscritor() {
         return escritor;
     }
 
     public String getNomeCliente() {
         return nomeCliente;
     }
-    private void ConexaoDireta(GerenciadorDeClientes destinatario) throws IOException {
-        while (true) {
-        	String msg = leitor.readLine();
-            try {
-                msg = leitor.readLine();
-                if (msg.equalsIgnoreCase("::SAIR")) {
-                    this.cliente.close();
-                    break;
-                }
-                destinatario.getEscritor().println(this.nomeCliente + " disse: " + msg);
-            } catch (IOException e) {
-                System.err.println("Erro na comunicação com o destinatário.");
-                e.printStackTrace();
-                break;
-            }
-        }
-    }
+    
 }
