fbd93594fff77ba7b31bcdee7971b547853545f5
Izaque Alves <105925377+izaqueAG@users.noreply.github.com>
2024-12-29 04:50:37 -0300

    add file

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Alô Mundo!");
+        System.out.println("Esse é o prototipo do Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int ano = 2022;
+        boolean inclusoNoPlano = true;
+        double notaFilme = 8.1;
+        
     }
 }
